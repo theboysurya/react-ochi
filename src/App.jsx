eb4473433eb2ab4f9df29d3089ace2af8a9e5527
theboysurya <thesuryasingh9@gmail.com>
2024-07-29 13:59:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className='w-full h-screen'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
